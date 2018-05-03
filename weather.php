5d4998f23f3d333a6491b6622bf7a433d2d6fdad
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2018-05-03 15:23:26 -0500

    323pm 5.3.18 updates

--- weather.php
+++ weather.php
@@ -1,6 +1,14 @@
 <?php
 
-$homepage = file_get_contents('https://www.weather-forecast.com/locations/Jackson/forecasts/latest');
-echo $homepage;
+$city=$_GET['city'];
+
+$city=str_replace(" ","",$city);
+
+$contents=file_get_contents("https://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+
+preg_match('/<span class="phrase">(.*?)<\/span>/s', $contents, $matches);
+
+echo $matches[1];
+
 
 ?>
