76e3545b8328a2f6c2763ec72de92d9d4944738b
Lawrence Lockhart <ldlockhart@gmail.com>
2019-04-17 12:44:57 -0500

    Update weather.php

--- weather.php
+++ weather.php
@@ -6,7 +6,7 @@ $city=str_replace(" ","",$city);
 
 $contents=file_get_contents("https://www.weather-forecast.com/locations/".$city."/forecasts/latest");
 
-preg_match('/3 Day Weather Forecast Summary:<\/b><span class="phrase">(.*?)</s', $contents, $matches);
+preg_match('/3 Day Weather Forecast Summary:<\/b><span class="phrase">(.*?)</span>', $contents, $matches);
 
 echo $matches[1];
 
