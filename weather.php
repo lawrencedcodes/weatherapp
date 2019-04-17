4d37aaee82c1b3ff4211862fd752129fd27895db
Lawrence Lockhart <ldlockhart@gmail.com>
2019-04-17 13:43:22 -0500

    Update weather.php

--- weather.php
+++ weather.php
@@ -6,7 +6,7 @@ $city=str_replace(" ","",$city);
 
 $contents=file_get_contents("https://www.weather-forecast.com/locations/Dallas/forecasts/latest");
 
-preg_match("<p class="b-forecast__table-description-content"><span class="phrase">(.*?)<\/span>/s", $contents, $matches);
+preg_match('/Weather Today <\/h2>(.*?)<\/p>/s', $contents, $matches);
 
 print_r($matches);
 
